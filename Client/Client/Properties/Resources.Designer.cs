12d945ff32b01514fef638d857e54d9737de9749
fedjaz <fedjaz5@gmail.com>
2021-03-06 21:19:30 +0300

    fixed resources

--- Client/Client/Properties/Resources.Designer.cs
+++ Client/Client/Properties/Resources.Designer.cs
@@ -233,16 +233,6 @@ namespace Client.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap ShadowBackground {
-            get {
-                object obj = ResourceManager.GetObject("ShadowBackground", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Wood {
             get {
                 object obj = ResourceManager.GetObject("Wood", resourceCulture);
