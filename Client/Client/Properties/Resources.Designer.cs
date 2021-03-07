5ae9be7a09587cd3042a0ba68cf63b10cf05ced1
fedjaz <fedjaz5@gmail.com>
2021-03-07 12:12:11 +0300

    Revert "fixed resources"
    This reverts commit 12d945ff32b01514fef638d857e54d9737de9749.

--- Client/Client/Properties/Resources.Designer.cs
+++ Client/Client/Properties/Resources.Designer.cs
@@ -233,6 +233,16 @@ namespace Client.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ShadowBackground {
+            get {
+                object obj = ResourceManager.GetObject("ShadowBackground", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Wood {
             get {
                 object obj = ResourceManager.GetObject("Wood", resourceCulture);
